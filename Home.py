9f425a56384f48737959521ba430b684da7e615e
mansi-manoj <56258881+mansi-manoj@users.noreply.github.com>
2022-12-06 07:57:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,8 +10,8 @@ if name:
     st.write("Hello ", name)
     stroke_title = '<p style="font-family:Courier; font-size: 15px;">Let us Talk about Stroke</p>'
     st.markdown(stroke_title, unsafe_allow_html=True)
-    img = cv.imread("stroke.png", 1)
-    image = np.array([img])
+    #img = cv.imread("stroke.png", 1)
+    #image = np.array([img])
     st.write("A stroke, sometimes called a brain attack, occurs when something blocks blood supply to part of the brain or when a blood vessel in the brain bursts. In either case, parts of the brain become damaged or die. A stroke can cause lasting brain damage, long-term disability, or even death.")
     st.write('Use the sidebar options to navigate the app,')
     st.write("Click on Data and it's Visualization to see how the data used for this app looks")
